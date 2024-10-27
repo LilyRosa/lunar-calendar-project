50fef2e586addb362f111253d071e9e839a3d12f
LilyRosa <135471998+LilyRosa@users.noreply.github.com>
2024-10-26 21:13:54 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Initial from "./components/Initial.vue";
+</script>
+
+<template>
+  <div>
+    <Initial />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Initial.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img src="" alt="" />
+  </div>
+
+  <div>
+    <h3>Nombre de la fase de la luna actual</h3>
+  </div>
+
+  <div>
+    <div>
+      <h4>Quedan x días de la fase actual</h4>
+    </div>
+
+    <div>
+      <h3>Próxima fase</h3>
+      <h4>"próxima fase"</h4>
+      <h4>fecha en la que empieza la próxima fase</h4>
+    </div>
+  </div>
+
+  <div>
+    <button>Ver Calendario</button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
